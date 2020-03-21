3329301c536470219fdf6ec584624b479da13d9f
mohonmia <mohonmia97@gmail.com>
2020-03-21 08:43:53 +0600

    first commit

---
+++ src/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyARju27iYT-hcD59uiD6jcRvelp3EhmrvA",
+  authDomain: "ema-john-simple-80ac8.firebaseapp.com",
+  databaseURL: "https://ema-john-simple-80ac8.firebaseio.com",
+  projectId: "ema-john-simple-80ac8",
+  storageBucket: "ema-john-simple-80ac8.appspot.com",
+  messagingSenderId: "74327590636",
+  appId: "1:74327590636:web:21e5ddc52c7b647c3f07c9"
+};
+
+export default firebaseConfig;
